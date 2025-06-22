4dc01116cbbba4bc889603b6f8f67019ea3e6249
huifanghf <huif@wzwzx.cn>
2025-06-22 15:03:27 +0800

    fix typo
    Change-Id: Ib1fdce5d16b3a121ff5ff9e62b1322b54402130f

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -27,7 +27,7 @@ PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
 ifeq ($(EXTHM_BUILD),)
-PRODUCT_COPY_FILES += \Add commentMore actions
+PRODUCT_COPY_FILES += 
     device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 endif
 
